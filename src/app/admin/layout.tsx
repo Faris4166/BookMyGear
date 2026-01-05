b0aeb60b36fadd6f60bc447d88ea3fe41486b1da
faris <farisarthan@gmail.com>
2026-01-05 16:06:33 +0700

    feat: Set up initial admin and user layouts with navigation, page redirects, and data files.

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,49 +1,14 @@
-import type { Metadata } from "next";
-import { ClerkProvider } from '@clerk/nextjs'
-import { Navbar } from "@/components/Navbar"
-import '../globals.css'
-import { Inter } from 'next/font/google'
-import { ThemeProvider } from "@/components/theme-provider"
-import { Geist, Geist_Mono } from "next/font/google";
+import { Navbar } from "@/components/Navbar";
 
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "BookMyGear ",
-  description: "Rent the best gear for your adventures.",
-};
-
-const inter = Inter({ subsets: ['latin'] })
-
-export default function RootLayout({
+export default function AdminLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
-        <body className={`${inter.className} ${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <Navbar />
-            <main className="min-h-screen bg-background">{children}</main>
-          </ThemeProvider>
-        </body>
-      </html>
-    </ClerkProvider>
-  )
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
 }
